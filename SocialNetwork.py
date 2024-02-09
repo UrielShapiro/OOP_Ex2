77da72bfef6e4bd09f5d2d9719430005d6686bdb
oriya <oriyas.07@gmail.com>
2024-02-09 15:42:04 +0200

    open the files for the assignment

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,5 +1,14 @@
 import User
 import Post
+
+
 class SocialNetwork:
+    users = []
+    posts = []
+
     def __init__(self, name):
         self.name = name
+
+    def sign_up(self, name, password):
+        if 4 < len(password) < 8:
+            self.users.add(User(name, password))
